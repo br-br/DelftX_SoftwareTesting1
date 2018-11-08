df87b92fab6ee4f930feda3d7789099b920b3f66
Britte <britte.breindl@gmail.com>
2018-11-08 18:35:35 +0100

    exercise mirror done

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,14 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+    @ParameterizedTest(name = "input={0}, result={1}")
+    @CsvSource(value = {"azerty,''", "azerta,a", "azertza,az", "azereza,azereza", "azeeza,azeeza"})
+    public void MirrorTests(String input, String expectedResult) {
+        String result = new Mirror().mirrorEnds(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
