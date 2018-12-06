dfdddbf7030fdd88cad8f5906fd5e4c34949402e
Britte <britte.breindl@gmail.com>
2018-12-06 23:13:27 +0100

    finished week 5

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -2,6 +2,20 @@ package tudelft.mirror;
 
 public class Mirror {
 
+/**
+ * Find the substring of a string that's mirrored from it's end.
+ * 
+ * Find zero or more characters at the very begining of the given string, 
+ * and at the very end of the string in reverse order (possibly overlapping)
+ *
+ * For example: mirrorEnds("abXYZba") returns "ab"
+ * For example: mirrorEnds("abXXXba") returns "abXXXba"
+ *
+ *
+ * @param string    non-null String
+ * 
+ * @return the substring of the string that's mirrored from it's end
+ */
     public String mirrorEnds(String string) {
         String mirror = "";
 
@@ -19,3 +33,4 @@ public class Mirror {
         return begin == end || begin == end + 1 ? string : mirror;
     }
 }
+
